39c29a9407fda2828baa849a55c0fe708e21b282
Jvildibill <46855504+Jvildibill@users.noreply.github.com>
2019-01-21 23:17:40 -0500

    finished#5
    Changed it to not need an input

--- problem5Euler.py
+++ problem5Euler.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 import math
 
-highend = input("What should the high end be?")
 y=0
+x=0
 count = 0
 
-for x in range(1,highend):
+while count == 0:
+	x = x+1
 	y = 20*x
 	if (y%19==0 and y%18==0 and y%17==0 and y%16==0 and y%15==0 and y%14==0 and y%13==0 and y%12==0 and y%11==0):
 		print(y)
 		count = 1
-		break
-if count==0:
-	print("Nope")
